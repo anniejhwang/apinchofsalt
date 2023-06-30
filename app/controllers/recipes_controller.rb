44828242555f3146bab589d5906aa441a8918c57
Annie <anniehwang428@gmail.com>
2023-06-30 15:07:42 -0400

    add show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,21 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     render :index
   end
+
+  def create
+    @recipe = Recipe.create(
+      title: params[:title],
+      ingredients: params[:ingredients],
+      prep_time: params[:prep_time],
+      cook_time: params[:name],
+      total_time: params[:name],
+      instructions: params[:name],
+    )
+    render :show
+  end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+    render :show
+  end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -8,4 +8,19 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Recipe.count, data.length
   end
+
+  test "create" do
+    assert_difference "Recipe.count", 1 do
+      post "/recipes.json", params: { title: "Americano", ingredients: "espresso, water, ice", prep_time: "1 min", cook_time: "2 mins", total_time: "3 mins", instructions: "Make an espresso and mix everything together with water and ice" }
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/recipes/#{Recipe.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "ingredients", "prep_time", "cook_time", "total_time", "instructions", "created_at", "updated_at"], data.keys
+  end
 end
